fd017878f9ef9df7577061afda34eebd3f9c9cbd
Alcenir Costa <aallcenir.9000@gmail.com>
2024-01-30 23:12:15 -0300

    adding dynamism to big-cards and small-cards

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
+  @Input() cover: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
